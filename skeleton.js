91c4df40f27218b00818ec7f41b6b2d06885bc71
John Yong <whipermr5@gmail.com>
2017-03-19 18:58:44 +0800

    Implement q2 stacked bar chart

--- skeleton.js
+++ skeleton.js
@@ -42,13 +42,15 @@ function loadQ1Data() {
 function loadQ2Data() {
   function parseRow(row) {
     return {
-      monthIndex: + row.monthIndex,
+      date: d3.timeParse('%m')(+ row.monthIndex + 1),
       additions: + row.additions,
-      deletions: + row.deletions
+      deletions: + row.deletions,
+      total: (+ row.additions) + (+ row.deletions)
     };
   }
 
   function onSuccess(data) {
+    data.sort(function(a, b) { return a.date - b.date; });
     q2 = data;
     console.log("Loaded q2 data");
     loadQ3Data();
@@ -73,13 +75,13 @@ function loadQ3Data() {
   readCsv("data/q3.csv", parseRow, onSuccess);
 }
 
-function makePieChart(dataset) {
+function makePieChart(containerSelector, dataset) {
   var width = 480;
   var height = 480;
   var radius = Math.min(width, height) / 2;
   var colour = d3.scaleOrdinal(d3.schemeCategory20c);
 
-  var svg = d3.select('#chart1')
+  var svg = d3.select(containerSelector)
               .append('svg')
               .attr('width', width)
               .attr('height', height)
@@ -116,6 +118,60 @@ function makePieChart(dataset) {
      .attr('dy', '2.5em');
 }
 
+function makeBarChart(containerSelector, dataset) {
+  var margin = {top: 20, right: 50, bottom: 30, left: 20};
+  var width = 960 - margin.left - margin.right;
+  var height = 500 - margin.top - margin.bottom;
+
+  var svg = d3.select(containerSelector)
+              .append('svg')
+              .attr('width', width + margin.left + margin.right)
+              .attr('height', height + margin.top + margin.bottom)
+              .append('g')
+              .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+  var x = d3.scaleBand().rangeRound([0, width]).padding(0.3).align(0.3);
+  var y = d3.scaleLinear().rangeRound([height, 0]);
+  var colour = ['#cb2431', '#2cbe4e'];
+
+  x.domain(dataset.map(function(d) { return d.date; }));
+  y.domain([0, d3.max(dataset, function(d) { return d.total; })]).nice();
+
+  var xAxis = d3.axisBottom(x).tickFormat(d3.timeFormat('%b'));
+  var yAxis = d3.axisRight(y).ticks(10, "s");
+
+  var stack = d3.stack();
+
+  svg.selectAll(".serie")
+     .data(stack.keys(dataset.columns.slice(1))(dataset))
+     .enter().append('g')
+       .attr('class', 'serie')
+       .style('fill', function(d, i) { return colour[i]; })
+       .selectAll('rect')
+       .data(function(d) { return d; })
+     .enter().append("rect")
+       .attr("x", function(d) { return x(d.data.date); })
+       .attr("y", function(d) { return y(d[1]); })
+       .attr("height", function(d) { return y(d[0]) - y(d[1]); })
+       .attr("width", x.bandwidth());
+
+  svg.append('g')
+       .attr("class", "axis axis--x")
+       .attr("transform", "translate(0," + height + ")")
+       .call(xAxis);
+
+  svg.append('g')
+       .attr("class", "axis axis--y")
+       .attr("transform", "translate(" + width + ",0)")
+       .call(yAxis)
+     .append("text")
+       .attr("dx", "-1em")
+       .attr("dy", "-1em")
+       .attr("text-anchor", "start")
+       .attr("fill", "#000")
+       .text("Lines of code");
+}
+
 // this function is called when all CSVs have been loaded successfully
 function onDataLoadingComplete(q1, q2, q3) {
   // main logic here
@@ -124,7 +180,8 @@ function onDataLoadingComplete(q1, q2, q3) {
   console.log(q2);
   console.log(q3);
 
-  makePieChart(q1);
+  makePieChart('#chart1', q1);
+  makeBarChart('#chart2', q2);
 }
 
 function main() {
